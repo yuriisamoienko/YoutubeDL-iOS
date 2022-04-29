14b0210c96c0e8ea3f3f38c07b0d45e7d1ed8037
yuriisamoienko <87826353+yuriisamoienko@users.noreply.github.com>
2022-04-29 21:02:58 +0300

    Update Package.swift
    updated package dependencies links to my fork repos

--- Package.swift
+++ Package.swift
@@ -10,9 +10,9 @@ let package = Package(
             targets: ["YoutubeDL"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/kewlbear/FFmpeg-iOS.git", .branch("main")),
-        .package(url: "https://github.com/kewlbear/PythonKit.git", .branch("master")),
-        .package(url: "https://github.com/kewlbear/Python-iOS.git", .branch("kivy-ios")),
+        .package(url: "https://github.com/yuriisamoienko/FFmpeg-iOS.git", .branch("main")),
+        .package(url: "https://github.com/yuriisamoienko/PythonKit.git", .branch("master")),
+        .package(url: "https://github.com/yuriisamoienko/Python-iOS.git", .branch("kivy-ios")),
     ],
     targets: [
         .target(
